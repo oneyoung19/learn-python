0a1f956a232dcad96fe192539ba7dc2c27b5171d
Andy Yang <2878847069@qq.com>
2025-03-24 15:03:34 +0800

    docs(1.var.py): 补充数据类型转换示例
    - 添加了多种数据类型转换的示例代码
    - 涵盖了 int、float、str、bool 类型之间的转换
    - 包含了二进制和十六进制转换的示例
    - 增加了字符和 ASCII 码转换的示例

--- 1.var.py
+++ 1.var.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+# -*- coding: utf-8 -*-
+# 如果是解释器是python而不是python3 那么需要额外声明上述两者coding之一
 """
 数据类型：
 int
@@ -16,3 +19,21 @@ dict
 
 较推荐snake_case命名 而不是camelCase命名
 """
+
+a = 100
+b = 123.45
+c = '123'
+d = '100'
+e = '123.45'
+f = 'hello, world'
+g = True
+print(float(a))         # int类型的100转成float，输出100.0
+print(int(b))           # float类型的123.45转成int，输出123
+print(int(c))           # str类型的'123'转成int，输出123
+print(int(c, base=16))  # str类型的'123'按十六进制转成int，输出291
+print(int(d, base=2))   # str类型的'100'按二进制转成int，输出4
+print(float(e))         # str类型的'123.45'转成float，输出123.45
+print(bool(f))          # str类型的'hello, world'转成bool，输出True
+print(int(g))           # bool类型的True转成int，输出1
+print(chr(a))           # int类型的100转成str，输出'd' 用于unicode码点值转换成具体文本 character的缩写
+print(ord('d'))         # str类型的'd'转成int，输出100 用于具体文本获取对应的unicode码点值 ordinal的缩写
